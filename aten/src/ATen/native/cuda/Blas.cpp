17384166b25b552bd4eebf66902f21f2874610e3
anjali411 <chourdiaanjali123@gmail.com>
2022-02-03 20:08:20 +0000

    Add ZT fastpath for torch.{mm, addmm, dot, vdot}
    ghstack-source-id: e4d1fce9e56db35ce47ffb7ed9187d542a39001c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/71129

--- aten/src/ATen/native/cuda/Blas.cpp
+++ aten/src/ATen/native/cuda/Blas.cpp
@@ -114,6 +114,20 @@ Tensor& addmm_out_cuda_impl(Tensor& result, const Tensor& self, const Tensor& ma
     TORCH_CHECK(self__sizes[1] == mat2_sizes[1], "self_ dim 1 must match mat2 dim 1");
   }
 
+  TORCH_CHECK(!result._is_zerotensor(), "ZeroTensors are immutable. Please use the materialized zero tensor ",
+                    "obtained using .clone() if you want a mutable tensor.");
+  if (mat1._is_zerotensor() || mat2._is_zerotensor()) {
+    return at::mul_out(
+        result,
+        self,
+        at::native::scalar_tensor(
+            beta,
+            self.scalar_type(),
+            c10::nullopt /* layout */,
+            at::kCPU,
+            c10::nullopt /* pin_memory */));
+  }
+
   if (&result != &self) {
     at::native::resize_output(result, self__sizes);
     if (beta.toComplexDouble() != 0.0) {
@@ -357,6 +371,10 @@ Tensor dot_cuda(const Tensor& self, const Tensor& other) {
     incy = 1;
   }
 
+if (self._is_zerotensor() || other._is_zerotensor()) {
+  return at::_efficientzerotensor({}, self.options());
+}
+
 return AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND2(
       ScalarType::Half, ScalarType::BFloat16,
       self.scalar_type(), "dot",
@@ -394,7 +412,12 @@ Tensor vdot_cuda(const Tensor& self, const Tensor& other) {
   }
 
   at::NoNamesGuard guard;
+  bool found_zerotensor = self._is_zerotensor() || other._is_zerotensor();
   dot_check(self, other);
+
+  if (self._is_zerotensor() || other._is_zerotensor()) {
+    return at::_efficientzerotensor({}, self.options());
+  }
 
   const int n = static_cast<int>(self.numel());
   int incx = static_cast<int>(self.stride(0));
